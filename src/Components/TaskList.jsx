d5f56d2da833e07b0dbb8237c14729412aec7876
Kush  Mevada <102648785+Kush2343@users.noreply.github.com>
2025-05-20 16:12:31 +0530

    Update TaskList.jsx

--- src/Components/TaskList.jsx
+++ src/Components/TaskList.jsx
@@ -21,7 +21,7 @@ function TaskList({ tasks, onUpdateTask, onDeleteTask }) {
   return (
     <ul className="divide-y divide-gray-200">
       {tasks.map((task) => (
-        <li key={task._id} className="p-4 hover:bg-gray-50">
+        <li key={task._id} className="p-4 hover:bg-gray-50 h-[225px] overflow-y-scroll">
           {editingId === task._id ? (
             <div className="space-y-4">
               <input type="text" value={editTitle} onChange={(e) => setEditTitle(e.target.value)}  className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
